6f4d38a2276e7d23eeab4e4c233bf7ec99e415a0
shalinieva <shaliniaruleshan@gmail.com>
2022-11-15 18:17:41 +0530

    First code added

---
+++ src/org/sort/SortArray.java
@@ -0,0 +1,15 @@
+package org.sort;
+
+import java.util.Arrays;
+
+public class SortArray {
+	public static void main(String[] args) {
+		int a[]={ 10,20,20,10,52,32,45,1,0};
+		Arrays.sort(a);
+		for(int i=0; i<a.length; i++)
+		{
+		    System.out.println(a[i]);
+		}
+	}
+
+}
