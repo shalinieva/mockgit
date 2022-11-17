3c8d921a6200c95490b95e9d396b1e469b6f2ea4
shalinieva <shaliniaruleshan@gmail.com>
2022-11-17 18:30:24 +0530

    I have cloned my first code with master

--- src/org/sort/SortArray.java
+++ src/org/sort/SortArray.java
@@ -4,12 +4,17 @@ import java.util.Arrays;
 
 public class SortArray {
 	public static void main(String[] args) {
+		// master coding
 		int a[]={ 10,20,20,10,52,32,45,1,0};
 		Arrays.sort(a);
 		for(int i=0; i<a.length; i++)
 		{
 		    System.out.println(a[i]);
 		}
+		
+		//my coding
+		
+		System.out.println(" I have cloned my code with master coding");
 	}
 
 }
